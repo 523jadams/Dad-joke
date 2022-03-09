8b60f610bfdd37b27034f65ca3579ff76aa67c51
523jadams <92735748+523jadams@users.noreply.github.com>
2022-03-09 11:57:17 -0500

    finsihed

---
+++ jakeScript.js
@@ -0,0 +1,8 @@
+function what() {
+  document.getElementById('edit').innerHTML = 'Adam Brosio!!!!';
+  console.log('its working');
+}
+
+function reset() {
+  document.getElementById("edit").innerHTML = "I dont know, what?";
+}
--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-<script type="text/javascript">
-window.onload=function(){
-var outerGifs = document.querySelectorAll('.jsFollow'),
-    innerGifs = document.querySelectorAll('.jsFollow > .innerGif'),
-    innerGifImages = document.querySelectorAll('.jsFollow > .innerGif > .image');
-window.onmousemove = rotateElems;
-function rotateElems(e) {
-  var mouseX = e.pageX,
-      mouseY = e.pageY;
-  for(var i=0; i < innerGifs.length; i++) {
-    var outerGif = outerGifs[i],
-        innerGif = innerGifs[i],
-        innerGifImage = innerGifImages[i],
-        centerX = outerGif.offsetLeft + (outerGif.clientWidth / 2),
-        centerY = outerGif.offsetTop + (outerGif.clientHeight / 2),
-        degree = Math.atan2(mouseX - centerX, - (mouseY - centerY) )*(180/Math.PI);
-    innerGif.style.webkitTransform = "rotate(" + degree + "deg)";
-    innerGif.style.transform = "rotate(" + degree + "deg)";
-    degree = -degree;
-    innerGifImage.style.webkitTransform = "rotate(" + degree + "deg)";
-    innerGifImage.style.transform = "rotate(" + degree + "deg)";
+< script type = "text/javascript" >
+  window.onload = function() {
+    var outerGifs = document.querySelectorAll('.jsFollow'),
+      innerGifs = document.querySelectorAll('.jsFollow > .innerGif'),
+      innerGifImages = document.querySelectorAll('.jsFollow > .innerGif > .image');
+    window.onmousemove = rotateElems;
+
+    function rotateElems(e) {
+      var mouseX = e.pageX,
+        mouseY = e.pageY;
+      for (var i = 0; i < innerGifs.length; i++) {
+        var outerGif = outerGifs[i],
+          innerGif = innerGifs[i],
+          innerGifImage = innerGifImages[i],
+          centerX = outerGif.offsetLeft + (outerGif.clientWidth / 2),
+          centerY = outerGif.offsetTop + (outerGif.clientHeight / 2),
+          degree = Math.atan2(mouseX - centerX, -(mouseY - centerY)) * (180 / Math.PI);
+        innerGif.style.webkitTransform = "rotate(" + degree + "deg)";
+        innerGif.style.transform = "rotate(" + degree + "deg)";
+        degree = -degree;
+        innerGifImage.style.webkitTransform = "rotate(" + degree + "deg)";
+        innerGifImage.style.transform = "rotate(" + degree + "deg)";
+      }
+    }
   }
-}
-}
-</script></head>
+</script>
